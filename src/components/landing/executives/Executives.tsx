d56792ad77f74a1e37ef1135919d69576a695868
sekani50 <mohammedadeoye0@gmail.com>
2024-05-23 09:52:21 -0800

    feat: updated

--- src/components/landing/executives/Executives.tsx
+++ src/components/landing/executives/Executives.tsx
@@ -7,7 +7,7 @@ import "aos/dist/aos.css";
 export function Executives() {
   return (
     <>
-      <div  className="w-full my-10 p-4 sm:my-12 flex flex-col lg:flex-row gap-12 items-center lg:items-start py-4 h-fit sm:py-10">
+      <div className="w-full my-10 p-4 sm:my-12 flex flex-col lg:flex-row gap-12 items-center lg:items-start py-4 h-fit sm:py-10">
         <div className=" w-full lg:w-[500px]  bg-obiman   relative h-[400px]  sm:h-[500px]">
           <div className="w-full h-full mt-8 pr-8">
             <img
@@ -25,24 +25,27 @@ export function Executives() {
           className="w-full lg:w-[50%] gap-y-6 lg:pt-10 flex flex-col items-center justify-center md:justify-start md:items-start"
         >
           <h2 className=" font-semibold text-base sm:text-2xl text-center md:text-start">
-            Meet the CEO
+            Obitum Jonah Emmanuel
           </h2>
 
-          <p className=" text-sm text-zinc-600 sm:text-base text-center md:text-start">
-            Obiman is your one-stop destination for a seamless and innovative
-            shopping experience. As a competitor in e-commerce and dispatch
-            services, we bring you a curated selection of the latest smart
-            products and cutting-edge solutions designed to enhance your
-            everyday life.
+          <p className="font-semibold text-sm border-b-2 border-obiman py-2 sm:text-lg">
+            Group President & CEO
           </p>
-          <p className="  text-sm text-zinc-600 sm:text-base text-center md:text-start">
-            At Obiman, we believe in the power of technology to transform lives.
-            We are constantly exploring new innovations and trends to bring you
-            the most up-to-date products and services.
+          <p className=" text-sm text-zinc-600 sm:text-base text-center md:text-start">
+            Obitum Jonah Emmanuel is the founder and Chief Executive Officer of
+            Obiman Group, a multifaceted conglomerate headquartered in Nigeria
+            with a global presence. Under his leadership, the company operates a
+            diverse portfolio of businesses in real estate, technology,
+            entertainment, and e-commerce. His vision is to harness the
+            synergies between these subsidiaries to deliver transformative
+            solutions and enrich communities worldwide.
           </p>
         </div>
       </div>
-      <div id="leadership" className="w-full my-10 sm:my-12 flex flex-col gap-6 sm:gap-8 items-center p-4 md:px-10 lg:px-20">
+      <div
+        id="leadership"
+        className="w-full my-10 sm:my-12 flex flex-col gap-6 sm:gap-8 items-center p-4 md:px-10 lg:px-20"
+      >
         <h2 className="border-b-2 border-obiman py-3 font-semibold text-base sm:text-2xl text-center">
           Executive Leaders
         </h2>
@@ -84,26 +87,24 @@ export function TeamWidget({
   title: string;
   delay: string;
 }) {
-
-    useEffect(() => {
-        Aos.init()
-    },[])
+  useEffect(() => {
+    Aos.init();
+  }, []);
 
   return (
-    <div 
-    data-aos="fade-up"
+    <div
+      data-aos="fade-up"
       data-aos-easing="ease-in-out"
       data-aos-duration="500"
-     data-aos-delay={delay}
-    className="w-full  sm:w-[300px] flex flex-col items-center relative">
+      data-aos-delay={delay}
+      className="w-full  sm:w-[300px] flex flex-col items-center relative"
+    >
       <div className="w-full  sm:w-[300px] h-[350px]">
         <img src={image} alt="" className="w-full h-full " />
       </div>
 
       <h3 className="text-base font-medium sm:text-lg mt-2">{name}</h3>
-      <h3 className="text-sm text-gray-500 sm:text-sm font-normal">
-        {title}
-      </h3>
+      <h3 className="text-sm text-gray-500 sm:text-sm font-normal">{title}</h3>
     </div>
   );
 }
